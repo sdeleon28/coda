d39f477dfea1e77604dccaae3adc715ce6e3f0b7
Santiago de Leon <sdeleon28@gmail.com>
2016-08-30 14:39:48 -0300

    Implemented `Key.make`

--- src/music/Key.js
+++ src/music/Key.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
+import Scale from './Scale';
+import Chord from './Chord';
 
+const MAJOR = 'major';
+const MINOR = 'minor';
 const MAJOR_KEYS_WITH_SHARPS = 'C G D A E B F# C#'.split(' ');
 const MINOR_KEYS_WITH_SHARPS = 'A E B F# C# G# D# A#'.split(' ');
 const MAJOR_KEYS_WITH_FLATS = 'F Bb Eb Ab Db Gb Cb'.split(' ');
 const MINOR_KEYS_WITH_FLATS = 'A D G C F Bb Eb Ab'.split(' ');
 const MAJOR_KEYS = [...MAJOR_KEYS_WITH_SHARPS, ...MAJOR_KEYS_WITH_FLATS];
 const MINOR_KEYS = [...MINOR_KEYS_WITH_SHARPS, ...MINOR_KEYS_WITH_FLATS];
+const MAJOR_KEY_CHORD_TYPES = 'M m m M M m dim'.split(' ');
+const MINOR_KEY_CHORD_TYPES = 'm dim M m m M M'.split(' ');
+
 
 const getRelativeMinor = note => {
   if (!_.includes(MAJOR_KEYS, note)) {
@@ -33,12 +40,39 @@ const getRelativeMajor = note => {
   }
 };
 
-const make = (root, quality) => ({
-  root,
-});
+
+const make = (root, quality) => {
+  if (quality === MAJOR) {
+    if (!_.includes(MAJOR_KEYS, root)) {
+      throw new TypeError(
+        `make(): First argument must be a major key: ${MAJOR_KEYS.join(' ')}`);
+    }
+    return {
+      root,
+      quality,
+      chords: _.zip(Scale.make(root, 'M').ascending, MAJOR_KEY_CHORD_TYPES)
+        .map(([note, chordType]) => Chord.make(note, chordType)),
+    }
+  } else if (quality === MINOR) {
+    if (!_.includes(MINOR_KEYS, root)) {
+      throw new TypeError(
+        `make(): First argument must be a minor key: ${MINOR_KEYS.join(' ')}`);
+    }
+    return {
+      root,
+      quality,
+      chords: _.zip(Scale.make(root, 'm').ascending, MINOR_KEY_CHORD_TYPES)
+        .map(([note, chordType]) => Chord.make(note, chordType)),
+    }
+  } else {
+    throw new TypeError('make(): Second argument must be either Key.MAJOR or Key.MINOR');
+  }
+};
 
 
 const Key = {
+  MAJOR,
+  MINOR,
   make,
   getRelativeMinor,
   getRelativeMajor,
--- src/music/tests/keys.test.js
+++ src/music/tests/keys.test.js
@@ -4,6 +4,29 @@ import Key from '../Key';
 
 describe('Key', () => {
   describe('make', () => {
+    it('should create the C major key properly', () => {
+      const { root, quality, chords } = Key.make('C', Key.MAJOR);
+      const chordIds = chords.map(x => x.id);
+      expect(root).to.equal('C');
+      expect(quality).to.equal(Key.MAJOR);
+      expect(chordIds).to.deep.equal('CM Dm Em FM GM Am Bdim'.split(' '));
+    });
+    it('should create the A minor key properly', () => {
+      const { root, quality, chords } = Key.make('A', Key.MINOR);
+      const chordIds = chords.map(x => x.id);
+      expect(root).to.equal('A');
+      expect(quality).to.equal(Key.MINOR);
+      expect(chordIds).to.deep.equal('Am Bdim CM Dm Em FM GM'.split(' '));
+    });
+    it('should error out when provided a non-standard major key root', () => {
+      expect(() => Key.make('D#', Key.MAJOR)).to.throw(TypeError);
+    });
+    it('should error out when provided a non-standard minor key root', () => {
+      expect(() => Key.make('Db', Key.MINOR)).to.throw(TypeError);
+    });
+    it('should error out when provided a bogus quality', () => {
+      expect(() => Key.make('C', 'bogus')).to.throw(TypeError);
+    });
   });
   describe('getRelativeMinor', () => {
     it('should return A as the relative minor of C', () => {
